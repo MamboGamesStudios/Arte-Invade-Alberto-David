9ad9b2bc133cc60a66abc84507efdf399f1eea7c
MamboGamesStudios <150476364+MamboGamesStudios@users.noreply.github.com>
2024-03-08 18:58:03 +0000

    Prototipo Finalizado

--- Assets/Scripts/InteractionHandlerScript.cs
+++ Assets/Scripts/InteractionHandlerScript.cs
@@ -1,6 +1,5 @@
 using Microsoft.MixedReality.Toolkit;
 using Microsoft.MixedReality.Toolkit.Input;
-
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -31,14 +30,34 @@ public class InteractionHandlerScript : MonoBehaviour,
 
         if (eventData.MixedRealityInputAction.Description == "Select")
         {
-            gameObject.transform.TransformSize(transform.localScale * 2);
-            gameObject.GetComponent<SimpleCollectibleScript>().Collect();
-            
-            
+            Debug.Log("Funciona");
+            if (gameObject.tag == "Menu")
+            {
+                SceneManager.LoadScene("Main");
+            }
+            else if (gameObject.tag == "Sky")
+            {
+                Debug.Log("Sky");
+                SceneManager.LoadScene("Skybox");
+            }
+            else if(gameObject.tag == "Timelapse")
+            {
+                SceneManager.LoadScene("Video_timelapse");
+            }
+            else if(gameObject.tag == "Credits")
+            {
+                SceneManager.LoadScene("Credits");
+            }
+            else if (gameObject.tag == "Video") {
+                Debug.Log("Video");
+                SceneManager.LoadScene("Video_canteras");
+            }
+            else if (gameObject.tag == "Matterport") {
+                Debug.Log("Matterport");
+                Application.OpenURL("https://my.matterport.com/show/?m=KWP67Z6gwTb");
+                Application.Quit();
+            }
         }
-
-        
-
     }
 
     void IMixedRealityInputActionHandler.OnActionEnded(BaseInputEventData eventData) { }
